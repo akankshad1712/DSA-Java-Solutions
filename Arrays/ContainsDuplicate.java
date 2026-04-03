91811a8042f289824c7e03c67ea3c9581f5d7fe5
Akanksha Deshmukh <146211849+akankshad1712@users.noreply.github.com>
2026-04-03 14:48:01 +0530

    Update ContainsDuplicate.java

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -1,1 +1,29 @@
+/*
+Problem: Contains Duplicate
+Link: https://leetcode.com/problems/contains-duplicate/
 
+Approach:
+Use a HashSet to track elements.
+If an element already exists in the set, return true.
+Otherwise, add it to the set.
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+}
